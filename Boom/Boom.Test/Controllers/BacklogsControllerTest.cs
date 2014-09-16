ea3d69fe96a908109f9f41d02320bd1d734ecd8b
abeggchr <cha@zuehlke.com>
2014-09-16 19:14:31 +0200

    Test project added

---
+++ Boom/Boom.Test/Controllers/BacklogsControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Boom.Controllers.Test
+{
+    public class BacklogsControllerTest
+    {
+        [Fact]
+        public void Get_SingleBacklog_BacklogReturned()
+        {
+            Assert.False(false);
+            // var controller = new BacklogsController();
+
+        }
+    }
+}
