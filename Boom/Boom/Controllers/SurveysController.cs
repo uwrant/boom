2e4a45feea76ad9abbac93b533d22098892f752f
Stefan Barthel <stefanbarthel@gmail.com>
2014-09-16 22:57:45 +0200

    Added basic SurveysController

---
+++ Boom/Boom/Controllers/SurveysController.cs
@@ -0,0 +1,30 @@
+using Boom.Domain;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Boom.Controllers
+{
+    public class SurveysController : Controller
+    {
+        // GET: /surveys/
+        public IActionResult Get()
+        {
+            var backlog = new Backlog();
+            backlog.Name = "TestBacklog";
+            backlog.Id = 1234;
+
+            return this.Json(new[] { new { Name = "Team Presentation" }, new { Name = "Team Event" } });
+        }
+
+        // GET: /surveys/{id}
+        public IActionResult Get(long id)
+        {
+            var backlog = new Backlog();
+            backlog.Name = "TestBacklog";
+            backlog.Id = id;
+
+            return this.Json(backlog);
+        }
+    }
+}
