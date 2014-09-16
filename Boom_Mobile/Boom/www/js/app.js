4a420c3a6c060508d7c91aefefe0151067ebdaa6
lehmamic <lehmamic@bluewin.ch>
2014-09-16 22:26:39 +0200

    Add the nugget.conf file

--- Boom_Mobile/Boom/www/js/app.js
+++ Boom_Mobile/Boom/www/js/app.js
@@ -40,7 +40,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       .state('tab.surveys', {
           url: '/surveys',
           views: {
-              'tab-dash': {
+              'tab-surveys': {
                   templateUrl: 'templates/tab-surveys.html',
                   controller: 'SurveysCtrl'
               }
@@ -87,7 +87,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/surveys');
 
 });
 
--- Boom_Mobile/Boom/www/js/controllers.js
+++ Boom_Mobile/Boom/www/js/controllers.js
@@ -12,4 +12,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope) {
+})
+
+.controller('SurveysCtrl', function($scope) {
 });
