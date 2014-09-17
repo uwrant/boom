dcb07f852a7efb20a952fe28d7de926b10f8e70b
Stefan Barthel <stefanbarthel@gmail.com>
2014-09-17 16:51:50 +0200

    Fixed all eslint issues

--- Boom_Mobile/www/js/commons/constants/constants.js
+++ Boom_Mobile/www/js/commons/constants/constants.js
@@ -1,20 +1,21 @@
-'use strict';
+(function() {
+    'use strict';
+    var AppConstants = angular.module('jet.commons.constants');
 
-var AppConstants = angular.module('jet.commons.constants');
+    // choose one of the following:
+    // -Mac
+    // -Windows
+    var backend = 'Windows';
 
-// choose one of the following:
-// -Mac
-// -Windows
-var backend = 'Windows';
+    switch (backend) {
+        case 'Mac':
+            // ----- Spring Backend Config
+            AppConstants.constant('REST_API_URL', 'http://localhost:5004');
+            break;
+        case 'Windows':
+            // ----- .NET Backend Config
+            AppConstants.constant('REST_API_URL', 'http://localhost:5001');
 
-switch(backend) {
-    case 'Mac':
-        // ----- Spring Backend Config
-        AppConstants.constant('REST_API_URL', 'http://localhost:5004');
-        break;
-    case 'Windows':
-        // ----- .NET Backend Config
-        AppConstants.constant('REST_API_URL', 'http://localhost:5001');
-
-        break;
-}
+            break;
+    }
+})();
--- Boom_Mobile/www/js/commons/restUtils/smartresource.js
+++ Boom_Mobile/www/js/commons/restUtils/smartresource.js
@@ -1,14 +1,15 @@
-'use strict';
+(function(){
+    'use strict';
+    angular.module('jet.commons.restUtilities')
 
-angular.module('jet.commons.restUtilities')
+    /**
+     * This service wraps the angular $resource service.
+     * it appends a base url to each request.
+     */
+        .factory('smartResource', function (REST_API_URL, $resource) {
+            return function (url, params, actions) {
+                return $resource(REST_API_URL + url, params, actions);
+            };
 
-/**
- * This service wraps the angular $resource service.
- * it appends a base url to each request.
- */
-    .factory('smartResource', function (REST_API_URL, $resource) {
-        return function (url, params, actions) {
-            return $resource(REST_API_URL + url, params, actions);
-        };
-
-    });
+        });
+})();
