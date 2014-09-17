e9ab7d5043e137f2392680421391f92a5c704222
ozuuuu <oliver.zuercher@gmail.com>
2014-09-17 10:43:20 +0200

    some js cleanup

---
+++ Boom/Boom/revealjs/js/factories.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var app = angular.module('boom');
+
+    app.factory("presentationStorage", function () {
+        'use strict';
+
+        var STORAGE_ID = "vortrag-storage";
+
+        return {
+            get: function () {
+                return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+            },
+
+            put: function (presentations) {
+                localStorage.setItem(STORAGE_ID, JSON.stringify(presentations));
+            }
+        };
+    });
+})();
