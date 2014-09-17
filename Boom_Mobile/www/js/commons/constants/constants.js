b51148f560a5c8e5adc49b4fd629bcc6a29f1c08
lehmamic <lehmamic@bluewin.ch>
2014-09-17 17:54:42 +0200

    Add custom deployment scripts; Add Azure to the constants.js

--- Boom_Mobile/www/js/commons/constants/constants.js
+++ Boom_Mobile/www/js/commons/constants/constants.js
@@ -5,16 +5,22 @@ var AppConstants = angular.module('jet.commons.constants');
 // choose one of the following:
 // -Mac
 // -Windows
+// -Azure
 var backend = 'Windows';
 
 switch(backend) {
     case 'Mac':
-        // ----- Spring Backend Config
+        // ----- Mac Backend Config
         AppConstants.constant('REST_API_URL', 'http://localhost:5004');
         break;
     case 'Windows':
-        // ----- .NET Backend Config
-        AppConstants.constant('REST_API_URL', 'http://localhost:5001');
+    // ----- .NET Backend Config
+    AppConstants.constant('REST_API_URL', 'http://localhost:5001');
+
+    break;
+    case 'Azure':
+        // ----- Azures Backend Config
+        AppConstants.constant('REST_API_URL', 'http://boom-test.azurewebsites.net/');
 
         break;
 }
