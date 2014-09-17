4b9c0cbc908cded3da2876192a13477dde380630
abeggchr <cha@zuehlke.com>
2014-09-17 09:50:47 +0200

    enable testing BacklogController

--- Boom/Boom.Test/Controllers/BacklogsControllerTest.cs
+++ Boom/Boom.Test/Controllers/BacklogsControllerTest.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Boom.Domain;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using System;
 using Xunit;
 
 namespace Boom.Controllers.Test
 {
+    // run "k test" in command line in Boom.Test folder to execute the tests
     public class BacklogsControllerTest
     {
         [Fact]
         public void Get_SingleBacklog_BacklogReturned()
         {
-            Assert.False(false);
-            // var controller = new BacklogsController();
+            // Arrange
+            var controller = new BacklogsController();
 
+            // Act
+            var backlogActionResult = controller.Get() as JsonResult;
+
+            // Assert
+            Assert.NotNull(backlogActionResult);
+            var backlogs = (Backlog[])backlogActionResult.Data;
+            Assert.Equal(1, backlogs.Length);
+            var backlog = backlogs[0];
+            Assert.Equal("TestBacklog", backlog.Name);
         }
     }
 }
