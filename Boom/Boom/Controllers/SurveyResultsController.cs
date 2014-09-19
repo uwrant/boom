9d510a2cf0f95abf0ffb5caf7e805b8eb8fc6a3b
abeggchr <cha@zuehlke.com>
2014-09-19 14:07:03 +0200

    draft of /result

--- Boom/Boom/Controllers/SurveyResultsController.cs
+++ Boom/Boom/Controllers/SurveyResultsController.cs
@@ -1,6 +1,7 @@
 ﻿using Boom.Domain;
 using Microsoft.AspNet.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
@@ -20,13 +21,41 @@ namespace Boom.Controllers
         // GET: /surveys/{surveyId}/result
         public IActionResult Get(long surveyId)
         {
-            var options = this.boomContext.Votes
+            var options = this.boomContext.SurveyOptions
+                .Where(o => o.Survey.Id == surveyId)
+                .ToList();
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            foreach (SurveyOption option in options)
+            {
+                result.Add(option.Description, 0);
+            }
+
+            var votes = this.boomContext.Votes
                 .Where(v => v.Participant.Survey.Id == surveyId)
                 .Include(v => v.Options)
                 .Include(v => v.Participant)
                 .ToList();
 
-            return this.JsonSerialized(options);
+            foreach (Vote vote in votes)
+            {
+                foreach (SurveyOption option in vote.Options)
+                {
+                    int count;
+                    result.TryGetValue(option.Description, out count);
+                    count++;
+                    result.Remove(option.Description);
+                    result.Add(option.Description, count);
+                }
+            }
+
+        //     [
+        //    {"Description":"value", "Count":1},
+        //{"Description":"value", "Count":1}
+        //    ]
+
+            return this.JsonSerialized(result);
         }
     }
 }
