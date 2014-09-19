3eec2510e255447a94644f341ee957d6e4233cab
Artsiom Tsernouski <uwrant@live.com>
2014-09-19 13:50:45 +0200

    Directly opening the barcode scanner on view open

--- Boom_Mobile/www/js/scanSurvey/scanSurveyCtrl.js
+++ Boom_Mobile/www/js/scanSurvey/scanSurveyCtrl.js
@@ -1,22 +1,24 @@
 angular.module('scanSurvey')
 
-    .controller('ScanSurveyCtrl', function($cordovaBarcodeScanner, $state) {
-        var vm = this;
+  .controller('ScanSurveyCtrl', function ($cordovaBarcodeScanner, $state) {
+    var vm = this;
 
-        vm.title = "Scan a QR Code for adding a survey";
+    vm.title = "Scan a QR Code for adding a survey";
 
-        vm.scanBarcode = function() {
-            $cordovaBarcodeScanner.scan().then(function(imageData) {
-                // Success! Barcode data is here
-                var backendUrl = imageData.text;
-                var idStartIndex = backendUrl.lastIndexOf("/") + 1;
-                var id = backendUrl.substr(idStartIndex);
-                $state.go('tab.survey-detail',{surveyId: id});
-            }, function(err) {
-                // An error occured. Show a message to the user
-                
-            });
-        };
+    vm.scanBarcode = function () {
+      $cordovaBarcodeScanner.scan().then(function (imageData) {
+        // Success! Barcode data is here
+        var backendUrl = imageData.text;
+        var idStartIndex = backendUrl.lastIndexOf("/") + 1;
+        var id = backendUrl.substr(idStartIndex);
+        $state.go('tab.survey-detail', {surveyId: id});
+      }, function (err) {
+        // An error occured. Show a message to the user
 
-        return vm;
-    });
+      });
+    };
+
+    vm.scanBarcode();
+
+    return vm;
+  });
