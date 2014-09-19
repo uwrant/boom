6784e96b7160fa3f62d7bbbc3826c4be47ccd94f
Stefan Barthel <stefanbarthel@gmail.com>
2014-09-19 10:39:29 +0200

    Included real surveys for results overview

--- Boom_Mobile/www/js/surveyResults/surveyResultCtrl.js
+++ Boom_Mobile/www/js/surveyResults/surveyResultCtrl.js
@@ -1,7 +1,7 @@
 angular.module('surveyResults')
 
-.controller('SurveyResultCtrl', function($scope, $stateParams, SurveyResults) {
-    $scope.friend = SurveyResults.get($stateParams.friendId);
+.controller('SurveyResultCtrl', function($scope, $stateParams, SurveyRest, SurveyResults) {
+    $scope.survey = SurveyRest.get({ id: $stateParams.surveyId });
     $scope.myData = [
         { label: 'Kart fahren', count: 12 },
         { label: 'Klettern', count: 4 },
