f9affb78c247a5a6492b8ff8eaa341a927029c08
ozuuuu <oliver.zuercher@gmail.com>
2014-09-19 12:16:24 +0200

    client side result calculation

--- Boom/Boom/revealjs/js/surveyResultCtrl.js
+++ Boom/Boom/revealjs/js/surveyResultCtrl.js
@@ -7,19 +7,27 @@
         var survey = SurveyOptionsService.getCurrentSurvey();
 
         VotesService.query({ surveyId: survey.Id }, function (data) {
-            var blub = [];
+            var votes = Enumerable.From(data).SelectMany(function (s) { return s.Options; }).ToArray();
+            var results = [];
 
-            data.forEach(function (vote) {
-
+            votes.forEach(function (vote) {
+                if (Enumerable.From(results).Any(function (p) { return p.Id == vote.Option.Id }) == false) {
+                    results.Add({ Id: vote.Option.Id, label: vote.Option.Description, count: vote.Weight });
+                } else {
+                    var result = Enumerable.From(results).First(function (p) { return p.Id == vote.Option.Id });
+                    result.Weight += vote.Weight;
+                }
             });
+
+            vm.results = Enumerable.From(results).OrderByDescending(function (e) { return e.count; }).ToArray();
 
         });
 
-        vm.results = Enumerable.From([
-            { label: 'Kart fahren', count: 12 },
-            { label: 'Klettern', count: 4 },
-            { label: 'Eis essen', count: 7 },
-            { label: 'Grillen', count: 2 },
-            { label: 'Lasertag', count: 19 }]).OrderByDescending(function (e) { return e.count; }).ToArray();
+        //vm.results = Enumerable.From([
+        //    { label: 'Kart fahren', count: 12 },
+        //    { label: 'Klettern', count: 4 },
+        //    { label: 'Eis essen', count: 7 },
+        //    { label: 'Grillen', count: 2 },
+        //    { label: 'Lasertag', count: 19 }]).OrderByDescending(function (e) { return e.count; }).ToArray();
     })
 })();
